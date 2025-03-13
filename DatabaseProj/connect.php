ee314b053d72c4272a7d540a744229fd572b9ea4
Brian W. <brianw83002@gmail.com>
2025-03-12 17:37:47 -0700

    Add files via upload

---
+++ DatabaseProj/connect.php
@@ -0,0 +1,14 @@
+<?php
+$serverName = "DESKTOP-MLDR1IG\SQLEXPRESS";  // Use double backslashes (\)
+$connectionOptions = [
+    "Database"=>"DemoDB",
+    "Uid"=>"something",
+    "PWD"=>"something"
+];
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if($conn==false)
+    die(print_r(sqlsrv_errors(), true));
+else echo 'CONNECTION SUCCESS!';
+
+
+?>
