64794d3c01958f707e5dc8dd49ac53a30137ce8d
Brian W. <brianw83002@gmail.com>
2025-03-12 18:00:08 -0700

    Update connect.php

--- DatabaseProj/connect.php
+++ DatabaseProj/connect.php
@@ -8,7 +8,7 @@ $connectionOptions = [
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 if($conn==false)
     die(print_r(sqlsrv_errors(), true));
-else echo 'CONNECTION SUCCESS!';
+else echo 'CONNECTION SUCCESS!<br>';
 
 
 ?>
